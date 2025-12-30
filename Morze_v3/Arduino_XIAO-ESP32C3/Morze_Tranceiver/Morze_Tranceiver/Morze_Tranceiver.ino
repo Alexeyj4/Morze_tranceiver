393282a7cd106b9ef4f52e5693701cdee9ab1a17
Alexey <alexeyj4@yandex.ru>
2025-12-30 20:30:59 +0300

    объединил tranceiver_receiver в tranceiver

---
+++ Morze_v3/Arduino_XIAO-ESP32C3/Morze_Tranceiver/Morze_Tranceiver/Morze_Tranceiver.ino
@@ -0,0 +1,29 @@
+#define SPEAKER_FREQUENCY 3200
+#define SPEAKER_REF_PIN 6 //D4
+#define SPEAKER_PIN 4 //D6
+#define BTN_REF_PIN 8 //D8
+#define BTN_IN_PIN 10 //D10
+
+void setup() {  
+  pinMode(SPEAKER_REF_PIN, OUTPUT);
+  pinMode(SPEAKER_PIN, OUTPUT);
+  digitalWrite(SPEAKER_REF_PIN, LOW);
+  digitalWrite(SPEAKER_PIN, LOW);
+  pinMode(BTN_REF_PIN, OUTPUT);//Btn ref+
+  pinMode(BTN_IN_PIN, INPUT_PULLDOWN);//Btn input
+  digitalWrite(BTN_REF_PIN, HIGH);    
+}
+
+
+void loop() {  
+  if(digitalRead(BTN_IN_PIN)==HIGH){
+    tone(SPEAKER_PIN, SPEAKER_FREQUENCY);
+  }
+  delay(10);
+  if(digitalRead(BTN_IN_PIN)==LOW){
+    noTone(SPEAKER_PIN);
+  }
+
+  
+
+}
